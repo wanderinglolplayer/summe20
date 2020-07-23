fef5fcfb8d517151fe8817c186da4841f2504680
William Hufnagle <42181391+billhufnagle@users.noreply.github.com>
2020-07-23 18:59:00 -0400

    Removed response body, just string response  with '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/whufnagle/demo/summer20/RestController.java
+++ src/main/java/whufnagle/demo/summer20/RestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     @GetMapping("/welcome/{name}")
-    public ResponseEntity<String> welcomeMethod(String name){
-        return new ResponseEntity("Welcome, " + name, HttpStatus.OK);
+    public String welcomeMethod(String name){
+        return "Welcome, " + name;
     }
 }
