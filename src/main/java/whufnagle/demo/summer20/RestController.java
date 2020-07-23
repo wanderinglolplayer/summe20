dc0f8ea01019604eb3f62763825561a427234a8f
William Hufnagle <42181391+billhufnagle@users.noreply.github.com>
2020-07-23 18:50:35 -0400

    fixing imports

--- src/main/java/whufnagle/demo/summer20/RestController.java
+++ src/main/java/whufnagle/demo/summer20/RestController.java
@@ -1,10 +1,8 @@
 package whufnagle.demo.summer20;
 
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.resource.HttpResource;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
