b2d9ebdda4ed93738f51ffb6e7f9bfd8006f0295
William Hufnagle <42181391+billhufnagle@users.noreply.github.com>
2020-08-18 22:49:34 -0400

    new stock api

---
+++ src/main/java/whufnagle/demo/summer20/StockController.java
@@ -0,0 +1,20 @@
+package whufnagle.demo.summer20;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class StockController {
+    String key = System.getProperty("STOCKKEY");
+
+    @GetMapping("/stock/{symbol}")
+    public String getStock(@PathVariable String symbol){
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForEntity(
+                "https://api.tiingo.com/tiingo/daily/"+symbol+"/prices?token="+key,String.class,
+                new HttpHeaders()).getBody();
+    }
+}
--- src/main/java/whufnagle/demo/summer20/WelcomeController.java
+++ src/main/java/whufnagle/demo/summer20/WelcomeController.java
@@ -1,10 +1,7 @@
 package whufnagle.demo.summer20;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
